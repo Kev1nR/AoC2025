085082531d2b94d36ce3f1c47a959be98c2aa68c
Kevin Roche <kevinroche@blueyonder.co.uk>
2025-12-28 07:58:08 +0000

    refactored to handle part 1 and 2 code

--- Day 1/process.fsx
+++ Day 1/process.fsx
@@ -7,10 +7,10 @@ open System
 
 type DialTurn = { Steps: int; Loops: int; CompOp: int -> int -> bool }
 
-let parseInput (line: string) : DialTurn =
+let parseInput (loopPassStrategy : int -> int) (line: string) : DialTurn =
     let directionChar = line.[0]
     let steps' = Int32.Parse(line.[1..])
-    let loops = steps' / 100
+    let loops = loopPassStrategy steps' // / 100
     let steps = steps' % 100
     let dialTurn =
         match directionChar with
@@ -19,21 +19,33 @@ let parseInput (line: string) : DialTurn =
         | _ -> failwithf "Invalid direction character: %c" directionChar
     dialTurn
 
-let dialReadings filePath =
+let part1 = parseInput (fun _ -> 0)
+let part2 = parseInput (fun s -> s / 100)
+
+let dialReadings parser filePath =
     let lines = ReadData.readLines filePath
     lines
-    |> Seq.map parseInput
+    |> Seq.map parser
     |> Seq.toList
     
-let rec processData pos zeroCount dialReadings =  
+let rec processData pos zeroCount ignoreZeroPass dialReadings =  
   match dialReadings with
   | []    -> zeroCount
   | h::t  -> 
     let newPos = ((+) pos h.Steps) % 100
-    let hit = if newPos = 0 || (pos <> 0 && h.CompOp newPos pos) then h.Loops + 1 else h.Loops
+    let hit = if ignoreZeroPass 
+              then 
+                if newPos = 0 then 1 else 0
+              else
+                if newPos = 0 || (pos <> 0 && h.CompOp newPos pos) 
+                then 
+                    h.Loops + 1 
+                else 
+                    h.Loops 
 
-    processData newPos (zeroCount + hit) t    
+    processData newPos (zeroCount + hit) ignoreZeroPass t    
 
 #time    
-filePath |> dialReadings |> processData 50 0 |> printfn "Number of times dial hits zero: %d"
+filePath |> dialReadings part1 |> processData 50 0 true |> printfn "Number of times dial hits zero: %d"
+filePath |> dialReadings part2 |> processData 50 0 false |> printfn "Number of times dial hits zero: %d"
 #time
